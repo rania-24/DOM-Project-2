7c42d1191301972bdfaed33430f3506597879c4a
rania-24 <jemlirania8@gmail.com>
2024-07-08 11:34:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+    console.log('DOM fully loaded and parsed');
+});
+
+
+const colorBox = document.getElementById('color-box');
+const changeColorBtn = document.getElementById('change-color-btn');
+
+function getRandomColor() {
+    
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+
+changeColorBtn.addEventListener('click', function() {
+    
+    colorBox.style.backgroundColor = getRandomColor();
+});
+
